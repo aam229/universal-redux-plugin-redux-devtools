cf390cf9a1d71b792fb2dafdcc2a9366fc02c01a
Alexandre Massonneau <alexandre.massonneau@webcbg.com>
2016-04-26 16:41:35 -0400

    Add support for new config

---
+++ config.js
@@ -0,0 +1,9 @@
+var environments = require('universal-redux/lib/hooks').environments;
+
+module.exports = {
+  environments: [
+    environments.CLIENT,
+    environments.SERVER,
+    environments.DEVELOPMENT
+  ]
+};
